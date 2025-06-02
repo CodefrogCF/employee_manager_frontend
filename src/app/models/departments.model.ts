019a4c939f041496c0fce252a98d5a90795e7021
HagenK42 <666.thurisaz.666@gmail.com>
2025-06-03 01:10:21 +0200

    updated model

--- src/app/models/departments.model.ts
+++ src/app/models/departments.model.ts
@@ -1,7 +1,7 @@
 export interface Department {
     id: number;
     department_name: string;
-    department_manager: string;
+    department_manager: number;
     department_budget: number;
     department_revenue: number;
     department_employees: Array<number>;
